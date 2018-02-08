0950a729aa55ffe7534cd230cac1aae7e6e81a36
WillyCoding <36282469+WillyCoding@users.noreply.github.com>
2018-02-08 23:58:55 +0100

    Update ElProblemaDeLaAsignacion.cpp

--- ElProblemaDeLaAsignacion.cpp
+++ ElProblemaDeLaAsignacion.cpp
@@ -5,6 +5,16 @@
 //  Created by Guillermo on 4/2/18.
 //  Copyright © 2018 Guillermo. All rights reserved.
 //
+//  El ejercicio consiste en una matriz b cuadratica tenemos x valores donde las filas son los trabajadores y las
+//  columnas son los trabajos, deberemos conseguir asignar el trabajo mas baratod de cada trabajador pero que no este 
+//  asignado ya, donde la asignacion la realizamos atraves de una matriz x de modo que por ejemplo para el caso:
+//  1   2   3
+//  1   2   3
+//  1   2   3
+//  La matriz X tendremos 
+//  1   0   0
+//  0   1   0
+//  0   0   1   
 
 #include <iostream>
 using namespace std;
